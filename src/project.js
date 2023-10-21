cb19fbf9b5265150d5f8a1bb9bc2370ab32cf24b
AkanshaPundir <akansha.pundir2308@gmail.com>
2023-10-21 18:12:18 +0530

    initial commit

---
+++ src/project.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './App.css';
+
+function Project() {
+  return (
+    <div className="Project">
+    <h1>BOOTCAMP DAY 1</h1>
+    <p>Akansha Pundir</p>
+    <p>Indraprastha College for Women</p>
+            
+        
+    </div>
+  );
+}
+
+export default Project;
